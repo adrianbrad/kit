49445e58906e2a71b479e3c490fa743782333991
brad <me@adrianbrad.com>
2019-07-31 14:43:34 +0300

    added tests

--- fbmes/webhook.go
+++ fbmes/webhook.go
@@ -6,40 +6,31 @@ import (
 	"net/http"
 )
 
-type Webhook struct {
-	verifyToken string
-}
+func VerificationHandler(verifyToken string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.Body.Close()
 
-func NewWebhook(verifyToken string) *Webhook {
-	w := &Webhook{
-		verifyToken: verifyToken,
+		challenge := r.URL.Query().Get("hub.challenge")
+		token := r.URL.Query().Get("hub.verify_token")
+		mode := r.URL.Query().Get("hub.mode")
+
+		if token != verifyToken || mode != "subscribe" {
+			http.Error(
+				w,
+				"wrong validation token",
+				http.StatusNotFound,
+			)
+			Debug(`VerificationHandler: invalid token passed, expected: "%s", received: "%s"`, verifyToken, token)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(challenge))
+		Debug("VerificationHandler: webhook verified")
 	}
-
-	Debug("started webhook using token: %s", verifyToken)
-
-	return w
 }
 
-func (wh *Webhook) Verification(w http.ResponseWriter, r *http.Request) {
-	challenge := r.URL.Query().Get("hub.challenge")
-	token := r.URL.Query().Get("hub.verify_token")
-	mode := r.URL.Query().Get("hub.mode")
-
-	if token != wh.verifyToken || mode != "subscribe" {
-		http.Error(
-			w,
-			"wrong validation token",
-			http.StatusNotFound,
-		)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(challenge))
-	Debug("Webhook verified")
-}
-
-func (*Webhook) MessageHandler() http.HandlerFunc {
+func MessageHandler() http.HandlerFunc {
 	type request struct {
 		Object string `json:"object"`
 		Entry  []struct {
@@ -48,18 +39,25 @@ func (*Webhook) MessageHandler() http.HandlerFunc {
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
 		var body request
 		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			Debug(`MessageHandler: error while decoding request body: "%s"`, err.Error())
 			return
 		}
 		if body.Object != "page" {
 			w.WriteHeader(http.StatusNotFound)
+			Debug(`MessageHandler: received invalid body.object: "%s"`, body.Object)
 			return
 		}
 
+		Debug("MessageHandler: successfully received message: %+v", body)
+
 		for _, entry := range body.Entry {
 			fmt.Println(entry.Messaging[0])
+			Debug("MessageHandler: processed entry: %+v", entry)
 		}
 	}
 }
--- fbmes/webhook_test.go
+++ fbmes/webhook_test.go
@@ -1,1 +1,70 @@
 package fbmes
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestVerificationHandler(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+	DEBUG = true
+	Logger = logrus.StandardLogger()
+
+	r, err := http.NewRequest("", "?hub.verify_token=test&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe", strings.NewReader(""))
+	require.Nil(t, err)
+	w := httptest.NewRecorder()
+
+	h := VerificationHandler("test")
+	h.ServeHTTP(w, r)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	body, err := w.Body.ReadString(0)
+	require.Equal(t, io.EOF, err)
+	assert.Equal(t, "CHALLENGE_ACCEPTED", body)
+
+	r, err = http.NewRequest("", "?hub.verify_token=fail&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe", strings.NewReader(""))
+	require.Nil(t, err)
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, r)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+	body, err = w.Body.ReadString('\n')
+	require.Nil(t, err)
+	assert.Equal(t, "wrong validation token\n", body)
+}
+
+func TestMessageHandler(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+	DEBUG = true
+	Logger = logrus.StandardLogger()
+
+	r, err := http.NewRequest("", "", strings.NewReader(`{"object": "page", "entry": [{"messaging": [{"message": "TEST_MESSAGE"}]}]}`))
+	require.Nil(t, err)
+	w := httptest.NewRecorder()
+	h := MessageHandler()
+
+	h.ServeHTTP(w, r)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	r, err = http.NewRequest("", "", strings.NewReader(`{"object"`))
+	require.Nil(t, err)
+	w = httptest.NewRecorder()
+
+	h.ServeHTTP(w, r)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	r, err = http.NewRequest("", "", strings.NewReader(`{"object": "fail"}`))
+	require.Nil(t, err)
+	w = httptest.NewRecorder()
+
+	h.ServeHTTP(w, r)
+	assert.Equal(t, http.StatusNotFound, w.Code)
+
+}
